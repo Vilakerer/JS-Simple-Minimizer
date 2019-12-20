0b475f0fa9b439d4cdc231a3e7343a89933c5358
Vilakerer <evoevo.diaz@gmail.com>
2019-12-20 15:14:24 -0600

    Initial commit

---
+++ js_minimizer.sh
@@ -0,0 +1,21 @@
+# simple javascript minification tool using google's closure compiler
+# this will pick up every .js file in the current directory and run it through the compiler
+# .min.js files can be found in a folder named "minified" inside the current directory
+# the closure-compiler*.jar structure avoids having to point latest date/version
+# By: Diego Diaz
+# checking if java is installed
+if command -v java
+then
+	# downloading latest closure-compiler
+	wget https://dl.google.com/closure-compiler/compiler-latest.zip
+	unzip compiler-latest.zip closure-compiler*.jar
+	for file in ./*.js
+	do
+		java -jar closure-compiler*.jar --use_types_for_optimization false --js "${file}" --js_output_file ./minified/"${file%.js}".min.js
+		echo "${file} was minified"
+	done
+	rm compiler-latest.zip closure-compiler*.jar
+else
+	echo "Java needs to be installed to run Google's closure compiler, please check https://www.java.com/en/download/ for instructions"
+fi
+exit
